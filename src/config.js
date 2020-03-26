e903f227321311e04ad9e6e2b1b9efd1b0a016f3
Patrick Palmer <ppalmer@pm.me>
2020-03-25 21:41:23 -0500

    Add react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  // s3: {
+  //   REGION: "us-east-1",
+  //   BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+  // },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://tv5v3ho19f.execute-api.us-east-1.amazonaws.com/prod/"
+  }
+  // cognito: {
+  //   REGION: "YOUR_COGNITO_REGION",
+  //   USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
+  //   APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
+  //   IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+  // }
+};
