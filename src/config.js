e77438fa18c7320ce845420d037cb80890222e24
Patrick Palmer <ppalmer@pm.me>
2020-03-27 13:28:49 -0500

    Set up Netlify

--- src/config.js
+++ src/config.js
@@ -1,16 +1,21 @@
-export default {
-  // s3: {
-  //   REGION: "us-east-1",
-  //   BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
-  // },
+// const dev = {
+//   apiGateway: {
+//     REGION: "us-east-1",
+//     URL: "https://tv5v3ho19f.execute-api.us-east-1.amazonaws.com/dev/"
+//   }
+// };
+
+const prod = {
   apiGateway: {
     REGION: "us-east-1",
     URL: "https://tv5v3ho19f.execute-api.us-east-1.amazonaws.com/prod/"
   }
-  // cognito: {
-  //   REGION: "YOUR_COGNITO_REGION",
-  //   USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
-  //   APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
-  //   IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
-  // }
+};
+
+// const config = process.env.REACT_APP_STAGE === 'prod' ? prod : dev;
+const config = prod;
+
+export default {
+  // Add common config values here
+  ...config
 };
